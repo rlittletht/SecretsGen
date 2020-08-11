422419345527237493cecf5e8d041100d1d10729
Rob Little <rlittletht@users.noreply.github.com>
2020-08-10 19:03:45 -0700

    add unit testing and create from stream or file

--- SecretsConfigReader.cs
+++ SecretsConfigReader.cs
@@ -1,15 +1,51 @@
 ﻿
+using System;
+using System.IO;
 using System.Xml;
 
 namespace SecretsGen
 {
-    public class SecretsConfigReader
+    public partial class SecretsConfigReader
     {
         public SecretsConfigReader() { }
 
-        public static SecretsFilesConfig CreateSecretsFilesConfig()
+        static SecretsFilesConfig CreateSecretsFilesConfigFromDom(XmlDocument dom)
         {
             return null;
+        }
+
+        public static SecretsFilesConfig CreateSecretsFilesConfig(string xml)
+        {
+            XmlDocument dom = new XmlDocument();
+            try
+            {
+                dom.LoadXml(xml);
+            }
+            catch (Exception e)
+            {
+                Exception eGeneric = new Exception(e.Message);
+
+                throw eGeneric;
+            }
+
+            return CreateSecretsFilesConfigFromDom(dom);
+        }
+
+        public static SecretsFilesConfig CreateSecretsFilesConfig(Stream stm)
+        {
+            XmlDocument dom = new XmlDocument();
+            try
+            {
+                dom.Load(stm);
+            }
+            catch (Exception e)
+            {
+                Exception eGeneric = new Exception(e.Message);
+
+                throw eGeneric;
+            }
+
+            return CreateSecretsFilesConfigFromDom(dom);
         }
     }
 }
---
+++ SecretsConfigReaderTest.cs
@@ -0,0 +1,27 @@
+﻿
+using System;
+using System.IO;
+using System.Xml;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+
+namespace SecretsGen
+{
+    public partial class SecretsConfigReader
+    {
+        [Test]
+        public static void TestCreateSecretsFilesConfig_InvalidXml()
+        {
+            // this should throw an exception
+            Assert.Throws<Exception>(() => CreateSecretsFilesConfig("<invalidXml>"));
+        }
+
+        [Test]
+        public static void TestCreateSecretsFilesConfig_Empty()
+        {
+            SecretsFilesConfig config = CreateSecretsFilesConfig("<secretsFilesConfig/>");
+
+            Assert.IsNotNull(config);
+        }
+    }
+}
