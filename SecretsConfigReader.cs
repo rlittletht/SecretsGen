8fc4529dc00cd80e15e28c9d077568843450a0dd
Rob Little <rlittletht@users.noreply.github.com>
2020-08-14 16:37:26 -0700

    parse root secretsFilesConfig and root secretsFileConfig

--- SecretsConfigReader.cs
+++ SecretsConfigReader.cs
@@ -2,6 +2,7 @@
 using System;
 using System.IO;
 using System.Xml;
+using XMLIO;
 
 namespace SecretsGen
 {
@@ -9,26 +10,73 @@ namespace SecretsGen
     {
         public SecretsConfigReader() { }
 
-        static SecretsFilesConfig CreateSecretsFilesConfigFromDom(XmlDocument dom)
+        static SecretsFilesConfig CreateSecretsFilesConfigFromXml(XmlReader xr)
         {
+            // if the config is empty, return null
+            if (!XmlIO.Read(xr))
+                return null;
+
+            XmlIO.SkipNonContent(xr);
+
+            if (xr.NodeType != XmlNodeType.Element)
+                throw new Exception("no root element");
+
+            if (xr.Name != "secretsFilesConfig")
+                throw new Exception("bad root element in secretsConfig");
+
+            SecretsFilesConfig filesConfig = new SecretsFilesConfig();
+
+            if (xr.IsEmptyElement)
+                return filesConfig; // just return an empty config
+
+            if (!XmlIO.Read(xr))
+                throw new Exception("can't find end of start root element");
+
+            XmlIO.SkipNonContent(xr);
+
+            while (xr.NodeType != XmlNodeType.Element)
+            {
+                // we don't expect any attributes, but we will be tolerant
+                if (xr.NodeType != XmlNodeType.Attribute)
+                    throw new Exception("non attribute on root element");
+
+                if (!XmlIO.Read(xr))
+                    throw new Exception("can't find end of start root element");
+
+                XmlIO.SkipNonContent(xr);
+            }
+
+            while (true)
+            {
+                if (xr.NodeType == XmlNodeType.Element)
+                {
+                    // now we are inside the root element
+                    if (xr.Name == "secretsFile")
+                    {
+                        //
+                    }
+                    continue;
+                }
+
+                if (xr.NodeType == XmlNodeType.EndElement)
+                {
+                    if (xr.Name != "secretsFilesConfig")
+                        throw new Exception($"end tag {xr.Name} does not match start tag <secretsFilesConfig>");
+
+                    break;
+                }
+                XmlIO.Read(xr);
+            }
+
             return null;
         }
 
         public static SecretsFilesConfig CreateSecretsFilesConfig(string xml)
         {
-            XmlDocument dom = new XmlDocument();
-            try
+            using (StringReader sr = new StringReader(xml))
             {
-                dom.LoadXml(xml);
+                return CreateSecretsFilesConfigFromXml(XmlReader.Create(sr));
             }
-            catch (Exception e)
-            {
-                Exception eGeneric = new Exception(e.Message);
-
-                throw eGeneric;
-            }
-
-            return CreateSecretsFilesConfigFromDom(dom);
         }
 
         public static SecretsFilesConfig CreateSecretsFilesConfig(Stream stm)
@@ -45,7 +93,7 @@ namespace SecretsGen
                 throw eGeneric;
             }
 
-            return CreateSecretsFilesConfigFromDom(dom);
+            return CreateSecretsFilesConfigFromXml(XmlReader.Create(stm));
         }
     }
 }
--- SecretsFileConfig.cs
+++ SecretsFileConfig.cs
@@ -1,12 +1,101 @@
 ﻿
+using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Xml;
+using NUnit.Framework;
+using XMLIO;
 
 namespace SecretsGen
 {
     class SecretsFileConfig
     {
         private Dictionary<string, string> m_mpPlaceholderToSecretID = new Dictionary<string, string>();
-
+        private string TargetFile { get; set; }
+        
         public SecretsFileConfig() { }
+
+        /*----------------------------------------------------------------------------
+        	%%Function: CreateSecretFileConfigFromXml
+        	%%Qualified: SecretsGen.SecretsFileConfig.CreateSecretFileConfigFromXml
+        	
+        ----------------------------------------------------------------------------*/
+        public static SecretsFileConfig CreateSecretFileConfigFromXml(XmlReader xr)
+        {
+            if (xr.Name == "secretsFileConfig")
+            {
+                if (xr.IsEmptyElement)
+                    return null;
+
+                // prepare read the attributes
+                if (!XmlIO.Read(xr))
+                    throw new Exception("can't unclosed secretFileConfig element");
+
+                XmlIO.SkipNonContent(xr);
+            }
+            else
+            {
+                throw new Exception("parsing secretsFileConfig without <secretsFileConfig>");
+            }
+
+            SecretsFileConfig fileConfig = new SecretsFileConfig();
+
+            // the reader should already be on the <secretFile>
+            while (true)
+            {
+                if (xr.NodeType == XmlNodeType.Element)
+                {
+                    throw new Exception($"unknown element {xr.Name}");
+                }
+
+                if (xr.NodeType == XmlNodeType.Attribute)
+                {
+                    if (xr.Name == "targetFile")
+                    {
+                        fileConfig.TargetFile = XmlIO.ReadGenericStringElement(xr, "targetFile");
+                        XmlIO.Read(xr);
+                        XmlIO.SkipNonContent(xr);
+                        continue;
+                    }
+                    else
+                    {
+                        throw new Exception($"bad attribute {xr.Name}");
+                    }
+                }
+            }
+        }
+
+        [Test]
+        public static void TestCreateSecretFileConfigFromXml_EmptyString()
+        {
+            SecretsFileConfig fileConfig;
+
+            Assert.Throws<Exception>(
+                () =>
+                {
+                    using (StringReader sr = new StringReader(""))
+                    {
+                        fileConfig = CreateSecretFileConfigFromXml(XmlReader.Create(sr));
+                    }
+                });
+        }
+
+        [Test]
+        public static void TestCreateSecretFileConfigFromXml_EmptyElement()
+        {
+            SecretsFileConfig fileConfig;
+
+            using (StringReader sr = new StringReader("<secretsFileConfig/>"))
+            {
+                XmlReader xr = XmlReader.Create(sr);
+                XmlIO.Read(xr);
+                XmlIO.SkipNonContent(xr);
+
+                fileConfig = CreateSecretFileConfigFromXml(xr);
+            }
+
+            Assert.IsNull(fileConfig);
+        }
+
     }
 }
